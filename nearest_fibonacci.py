45b4342b48cdae82d00acc4490f5c80ef33148ee
Raghuvarma417 <109516517+Raghuvarma417@users.noreply.github.com>
2022-10-11 22:28:59 +0530

    n=int(input()) x=0 y=1 while(1):     if(n<x):         if(abs(n-y)>abs(x-n)):             print(x)         elif(abs(n-y)==abs(x-n)):             print(y,x)         else:             print(y)         break     t=x     x=x+y     y=t

---
+++ nearest_fibonacci.py
@@ -0,0 +1,15 @@
+n=int(input())
+x=0
+y=1
+while(1):
+    if(n<x):
+        if(abs(n-y)>abs(x-n)):
+            print(x)
+        elif(abs(n-y)==abs(x-n)):
+            print(y,x)
+        else:
+            print(y)
+        break
+    t=x
+    x=x+y
+    y=t
